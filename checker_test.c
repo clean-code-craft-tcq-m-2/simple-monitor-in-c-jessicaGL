4898e31e407183f61b7c5c417e39460bd16481c6
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-20 18:40:23 -0600

    Update checker_test.c
    Adding the test to validate the measurements

--- checker_test.c
+++ checker_test.c
@@ -14,6 +14,27 @@ int checkSoc;
 
 char error[15];
 
+int ValidateMeasure(float value, float Max, float Min)
+{
+    int high_delta, low_delta;
+    
+    high_delta = abs(value - Max);
+    low_delta = abs(Min - value);
+   
+    if((high_delta | low_delta) > threshold)
+    {
+        printf("ERROR: Invalid meassuremnts, check the sensors.\n");
+        return 1;
+    }
+    
+    
+    else
+    {
+        printf("WARNING: meassuremnts exceed the max and min levels, but are in threshold range\n");
+        return 0;
+    }
+}
+
 int batteryIsOk(int checkChargeRate, int checkSoc, int checkTemperature) {
   if(checkChargeRate && checkSoc && checkTemperature)
   {
@@ -41,20 +62,25 @@ int CheckRangeForEverything( float value, float Max, float Min, char Testedparam
   }
   else
   {
-     printError(Testedparameter);
-     return 0;
+    ValidateMeasure(value, Max, Min);
+    printError(Testedparameter);
+    return 0;
    }
 
 }
 
 int main() {
-
+  
+  printf("**********TESTS*********\n")
   assert(batteryIsOk(0, 1, 0) == 0);
   assert(batteryIsOk(1,1,1)== 1);
   assert(CheckRangeForEverything(25,MaxChargeRate, MinChargeRate, "ChargeRate"));
   assert(CheckRangeForEverything(50, MaxSoc, MinSoc, "Soc"));
   assert(CheckRangeForEverything(15, MaxTemp, MinTemp, "Temperature"));
   assert(CheckRangeForEverything(40, 40, 30, "Temperature"));
+  assert(ValidateMeasure(55, 52, 40) == 0);
+  assert(ValidateMeasure(55, 49, 40) == 1);
+  assert(ValidateMeasure(55, 55, 55) == 1);
   
  
 }
