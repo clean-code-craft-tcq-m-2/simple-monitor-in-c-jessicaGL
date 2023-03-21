201fd246be07cfc6163c13c85751a314ef73dda5
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-20 18:25:18 -0600

    Update checker.c
    Adding a function to check abnormal measure

--- checker.c
+++ checker.c
@@ -7,6 +7,7 @@
 #define MinSoc 20.0
 #define MaxChargeRate 0.8
 #define MinChargeRate 0.0
+#define threshold 4.99
 
 int checkTemperature;
 int checkChargeRate;
@@ -14,14 +15,36 @@ int checkSoc;
 
 char error[15];
 
+int ValidateMeasure(float value, float Max, float Min)
+{
+    int high_delta, low_delta;
+    
+    high_delta = abs(value - Max);
+    low_delta = abs(Min - value);
+   
+    if((high_delta | low_delta) > threshold)
+    {
+        printf("ERROR: Invalid meassuremnts, check the sensors.\n");
+        return 1;
+    }
+    
+    
+    else
+    {
+        printf("WARNING: meassuremnts exceed the max and min levels, but are in threshold range\n");
+        return 0;
+    }
+}
+
 int batteryIsOk(int checkChargeRate, int checkSoc, int checkTemperature) {
   if(checkChargeRate && checkSoc && checkTemperature)
   {
-    printf("The battery is NOT_OK");
+    printf("The battery is OK\n");
     return 1;
   }
   else
   {
+    printf("The battery is NOT_OK\n");
     return 0;
   }
   
@@ -29,19 +52,21 @@ int batteryIsOk(int checkChargeRate, int checkSoc, int checkTemperature) {
 
 void printError( char error[])
 {
-  printf("Error in parameter: %s, Value out of range", error);
+  printf("Error in parameter: %s\n", error);
 }
 
 int CheckRangeForEverything( float value, float Max, float Min, char Testedparameter[])
 {
   if( value > Min && value < Max)
   {
-    return 0;
+    printf("Passed parameter: %s\n", Testedparameter);
+    return 1;
   }
   else
   {
-     printError(Testedparameter);
-     return 1;
+    ValidateMeasure(value, Max, Min);
+    printError(Testedparameter);
+    return 0;
    }
 
 }
@@ -50,6 +75,4 @@ int main() {
   checkChargeRate = CheckRangeForEverything(25,MaxChargeRate, MinChargeRate, "ChargeRate");
   checkSoc = CheckRangeForEverything(50, MaxSoc, MinSoc, "Soc");
   checkTemperature = CheckRangeForEverything(15, MaxTemp, MinTemp, "Temperature");
-  assert(batteryIsOk(0, 1, 0) == 0);
-  assert(batteryIsOk(1,1,1)== 1);
 }
